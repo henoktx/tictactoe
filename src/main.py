70a30f53c83a82b1a722496f3d463d950d1f0b4f
Henok Teixeira <henokteixeira@gmail.com>
2022-11-28 12:02:51 -0300

    refactor: input error prevention

--- src/main.py
+++ src/main.py
@@ -2,9 +2,15 @@ from game_logic import tictactoe
 
 jogo = tictactoe.Game()
 
-resp = input("Você deseja iniciar um servidor ou se conectar?(s/c) ")
+while True:
+    resp = input("Você deseja iniciar um servidor ou se conectar?(s/c) ")
 
-if resp == "s":
-    jogo.iniciar_server()
-else:
-    jogo.conectar_jogo()
+    if resp == "s":
+        jogo.iniciar_server()
+        break
+    elif resp == "c":
+        jogo.conectar_jogo()
+        break
+    else:
+        print("Não entendi :/\n")
+        continue
