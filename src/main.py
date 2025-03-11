442fdadd4b80db3be1f6dfae617ceaab05fb1587
Henok Teixeira <henokteixeira@gmail.com>
2025-03-11 15:11:14 -0300

    refactor: update main to use refactored code

--- src/main.py
+++ src/main.py
@@ -1,16 +1,28 @@
-from game_logic import tictactoe
+from game.controller import GameController
 
-jogo = tictactoe.Game()
+def main():
+    controller = GameController()
+    controller.view.clear()
 
-while True:
-    resp = input("Você deseja iniciar um servidor ou se conectar?(s/c) ")
+    print("Jogo da Velha 3D")
+    print("1. Hostear jogo")
+    print("2. Conectar a um jogo")
+    choice = input("Escolha uma opção: ")
 
-    if resp == "s":
-        jogo.iniciar_server()
-        break
-    elif resp == "c":
-        jogo.conectar_jogo()
-        break
+    if choice == "1":
+        controller.host_game()
+    elif choice == "2":
+        servers = controller.discovery_client.find_servers()
+        if servers:
+            print("\nServidores encontrados:")
+            for i, ip in enumerate(servers):
+                print(f"{i + 1}. {ip}")
+            selection = int(input("Escolha um servidor: ")) - 1
+            controller.join_game(servers[selection])
+        else:
+            print("Nenhum servidor encontrado")
     else:
-        print("Não entendi :/\n")
-        continue
+        print("Opção inválida")
+
+if __name__ == "__main__":
+    main()
